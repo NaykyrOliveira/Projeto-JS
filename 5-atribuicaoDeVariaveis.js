2e7eb6dc3fb10309b3d48b29a778108951bf3569
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-10-27 15:19:58 -0300

    Atribuições de variáveis

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 29;
+const primeiroNome = "João ";
+const sobrenome = "String ";
+
+// console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
