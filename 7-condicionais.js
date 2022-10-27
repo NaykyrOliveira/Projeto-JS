c7b8a0aa1dc861b2a800e35ecc79183b42e74f37
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-10-27 16:52:11 -0300

    Operadores lógicos

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -8,19 +8,34 @@ const listaDeDestinos = new Array(
 
 const idadeComprador = 17;
 const estaAcompanhada = true;
+const temPassagemComprada = true;
 console.log("Destinos possíveis: ");
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1); // remover itens da lista
-} else if(estaAcompanhada == true) {
-    console.log("Comprador está acompanhado");
-    listaDeDestinos.splice(1,1); // remover itens da lista
+// if(idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1,1); // remover itens da lista
+// } else if(estaAcompanhada == true) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1,1); // remover itens da lista
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if(
+    idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem!");
+    listaDeDestinos.splice(2,1); // remover itens da lista
 } else {
     console.log("Não é maior de idade e não posso vender");
 }
 
+console.log("Embarque: \n\n");
+if(idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa viagem!");
+} else {
+    console.log("Você não pode embarcar");
+}
 
 console.log(listaDeDestinos);
 
