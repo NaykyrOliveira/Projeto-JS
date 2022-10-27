45a3ad1d608d79bb5aba8fbae49daec36df237a6
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-10-27 15:40:36 -0300

    listas e adicionando itens

---
+++ listas.js
@@ -0,0 +1,16 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+);
+
+listaDeDestinos.push(`Brasília`); // adicionando item na lista depois dela ser declarada
+
+console.log("Destinos possíveis: ");
+// console.log(salvador, saoPaulo, rioDeJaneiro);
+console.log(listaDeDestinos);
