fd48254870337e28e8cf7deb887beb6befb546db
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-10-31 09:24:45 -0300

    loops (for)

--- 8-loops.js
+++ 8-loops.js
@@ -6,10 +6,10 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`,
 );
 
-const idadeComprador = 17;
-const estaAcompanhada = true;
+const idadeComprador = 18;
+const estaAcompanhada = false;
 let temPassagemComprada = false;
-const destino = "Salvador";
+const destino = "Brasília";
 
 console.log("\n Destinos possíveis: ");
 console.log(listaDeDestinos);
@@ -18,12 +18,24 @@ const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
 let destinoExiste = false;
-while(contador < 3) {
-    if(listaDeDestinos[contador] == destino) {
-        destinoExiste = true
-        break;
-    }
-    contador += 1;
-}
+// while(contador < 3) {
+//     if(listaDeDestinos[contador] == destino) {
+//         destinoExiste = true
+//         break;
+//     }
+//     contador += 1;
+// }
 
 console.log("Destino existe: ", destinoExiste);
+
+if(podeComprar && destinoExiste) {
+    console.log("Boa Viagem!");
+} else {
+    console.log("Desculpe tivemos um erro!");
+}
+
+for(let cont = 0; cont <3 ;cont++ ) {
+        if(listaDeDestinos[contador] == destino) {
+            destinoExiste = true
+        }
+    }
